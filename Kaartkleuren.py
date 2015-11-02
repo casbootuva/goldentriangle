52722748d42f0534addbf2a0838aa6fd3e14526d
Alwin <lijdsmanalwin@gmail.com>
2015-11-02 17:18:41 +0100

    Initial commit

---
+++ Kaartkleuren.py
@@ -0,0 +1,46 @@
+# Lonneke Lammers, Cas Boot, Alwin Lijdsman
+# Heuristieken
+# Kaartkleuren
+#
+#
+# Last update: 2 november 2015 door Alwin
+
+# class: land
+    """ attribute
+    aangrenzende landen: array van classes
+    kleur (integer tussen 0 en oneindig)     kan dat leeg zijn????
+    """
+
+# aray van landen (classes)
+
+""" mag weg:
+# Kaart 3: kaart met 3 landen alleen aan elkaar
+kaart3 = {"erie":["crawford", "warren"],
+          "warren":["erie", "crawford"],
+          "crawford":["erie", "warren"]}
+"""
+
+# algoritme om te bepalen hoeveel kleuren er minimaal nodig zijn
+
+
+for provincie in range (0, arrayLanden)
+        i = 0
+	if land(class) raakt landen waar colour != rood(0)
+		land.colour = i
+		i++
+		return to begin
+
+	    
+	else if provincie raakt landen waar colour != geel
+		land.colour = i
+		i = i + 2
+	else if i raakt landen waar colour != groen
+		colour = groen
+		return begin
+	else if i raakt landen waar colour != oranje
+		colour = oranje
+		return begin
+
+# print visualisatie
+
+
