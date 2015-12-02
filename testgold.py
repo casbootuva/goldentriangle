474468a0cd243d9882605f7d5a590420a6e5be18
Alwin <lijdsmanalwin@gmail.com>
2015-12-02 19:07:58 +0100

    Oplossing heuristiek

--- testgold.py
+++ testgold.py
@@ -1,99 +1,139 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+# Changes made to adjust to alwins computer, changes to code, only number pulled out
+#
+#
+#
+#
+#
+#
 
-class Province():
-    """
-    Een leeg vak op een kaart
-    """
-    def __init__(self, name):
-        self.name = name
-        self.neighbors = []
-        self.color = "?"
+# VOOR VISUALISATIE
+# import networkx as nx
+# import matplotlib.pyplot as plt
 
-# append etc. hier toevoegen
-    def addNeighbor(self, neighbor):
-        self.neighbors.append(neighbor)
 
-    def checkColor(self, colorCountry):
-        colorChecker = True
-        for neighbor in self.neighbors:
-            if colorCountry == neighbor.color:
-                colorChecker = False
-                return False
-        return True
+orderedDictionary = {}
+colorAmountList = []
 
-    def setColor(self):
-        for color in arrayColor:
-            if self.checkColor(color) == True:
-                self.color = color
-                break
+for i in range (0, 1000):
     
-    def visualize(self):
-    	nodes.append(self.name)
-    	colors.append(self.color)
-    	for neighbor in self.neighbors:
-    		edges.append((self.name, neighbor.name)) 
+    class Province():
+        """
+        Een leeg vak op een kaart
+        """
+        def __init__(self, name):
+            self.name = name
+            self.neighbors = []
+            self.color = "?"
+
+    # append etc. hier toevoegen
+        def addNeighbor(self, neighbor):
+            self.neighbors.append(neighbor)
+
+        def checkColor(self, colorCountry):
+            colorChecker = True
+            for neighbor in self.neighbors:
+                if colorCountry == neighbor.color:
+                    colorChecker = False
+                    return False
+            return True
+
+        def setColor(self):
+            for color in arrayColor:
+                if self.checkColor(color) == True:
+                    self.color = color
+                    break
+        
+        def visualize(self):
+        	nodes.append(self.name)
+        	colors.append(self.color)
+        	for neighbor in self.neighbors:
+        		edges.append((self.name, neighbor.name)) 
+
+    # DIT IS OOK VOOR VISUALISATIE
+    # g = nx.Graph()
+
+    edges = []
+    colors = []
+    nodes = []
+
+    arrayColor = ["0", "1", "2", "3", "4", "5", "6"]
+    countryDictionary = {}
+    with open("codes.csv", "r") as landcodes:
+        lines = landcodes.readlines()
+        for line in lines: 
+            s = line.split(",")
+            countryDictionary[s[0]] = Province(str(s[1]))
+    #print "this is the CountryDictionary", countryDictionary
+    # Maarten: als we de dictionary willen orderen, moeten we ze eerst nog sorteren, nu automatisch los door hashing van dictionary.
+
+    neighborDictionary = {}
+    with open("neighbors.csv", "r") as neighbors:
+        rows = neighbors.readlines()
+        for row in rows: 
+            n = row.split(",")
+            neighborDictionary[n[0]] = n[1:-1]
+    #print "this is the neighborDictionary", neighborDictionary
+     
+    for n in countryDictionary:
+        for m in neighborDictionary:
+            if m == n:
+                for i in neighborDictionary.get(m):
+                    (countryDictionary.get(n)).addNeighbor(countryDictionary.get(i))
 
 
-g = nx.Graph()
+    colorList = []
+    #emergency test for typo's
+    #counter = 0
 
-edges = []
-colors = []
-nodes = []
 
-arrayColor = ["red", "orange", "white", "yellow", "green", "purple", "blue"]
-countryDictionary = {}
-with open("codes.csv", "r") as landcodes:
-    lines = landcodes.readlines()
-    for line in lines: 
-        s = line.split(",")
-        countryDictionary[s[0]] = Province(str(s[1]))
-#print "this is the CountryDictionary", countryDictionary
-# Maarten: als we de dictionary willen orderen, moeten we ze eerst nog sorteren, nu automatisch los door hashing van dictionary.
+    # order the country dictionary
+    from random import shuffle
 
-neighborDictionary = {}
-with open("neighbors.csv", "r") as neighbors:
-    rows = neighbors.readlines()
-    for row in rows: 
-        n = row.split(",")
-        neighborDictionary[n[0]] = n[1:-1]
-#print "this is the neighborDictionary", neighborDictionary
- 
-for n in countryDictionary:
-    for m in neighborDictionary:
-        if m == n:
-            for i in neighborDictionary.get(m):
-                (countryDictionary.get(n)).addNeighbor(countryDictionary.get(i))
+    orderedCountryList = []
+    for i in countryDictionary.keys():
+        orderedCountryList.append(i)
 
-#emergency test for typo's
-#counter = 0
-for j in countryDictionary:
-    #print j
-    #print countryDictionary[j]
-    #print countryDictionary[j].neighbors
-    #counter += 1
-    #print counter
-    (countryDictionary.get(j)).setColor()
-    print countryDictionary.get(j).color
+    shuffle(orderedCountryList)
+    #print orderedCountryList
 
-for country in countryDictionary.values():
-	country.visualize()
+    # setcolor
+    for j in orderedCountryList:
+        #print j
+        #print countryDictionary[j]
+        #print countryDictionary[j].neighbors
+        #counter += 1
+        #print counter
+        (countryDictionary.get(j)).setColor()
+        # IS VOOR PRINTEN VAN KLEUREN
+        colorList.append(countryDictionary.get(j).color)
+        #print countryDictionary.get(j).color
+    colorAmount = max(colorList)
 
-g.add_nodes_from(nodes)
-g.add_edges_from(edges)
+    # prints the minmal amount of numbers
+    colorAmountList.append(colorAmount)
 
-position = nx.random_layout(g)
-nx.draw_networkx_labels(g,position)
+    # prints te values and colors when the lowest coloramount has been found (in this case three)
+    if colorAmount == "3": 
+        orderedDictionary[colorAmount] = orderedCountryList
+        print orderedDictionary
+        for values in orderedDictionary.get("3"):
+            instance = countryDictionary.get(values)
+            print instance.name + " " + instance.color
 
-nx.draw_random(g, node_color=colors)
+#   DIT IS DE VISUALISATIE
+# g.add_nodes_from(nodes)
+# g.add_edges_from(edges)
 
-plt.show()
+# position = nx.random_layout(g)
+# nx.draw_networkx_labels(g,position)
+
+# nx.draw_random(g, node_color=colors)
+
+# plt.show()
 
 
 
-
-
-
+#HIER WERD SOWIESO NIETS MEE GEDAAN
 # G=nx.Graph()
 
 # G.add_edge(1, 3)
