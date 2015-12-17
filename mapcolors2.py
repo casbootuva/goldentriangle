e94b88f65c0c67d2cd05e41933118c1d027613ac
Alwin <lijdsmanalwin@gmail.com>
2015-12-17 23:10:59 +0100

    opgeschoonde final code - Alwin

--- mapcolors2.py
+++ mapcolors2.py
@@ -12,19 +12,20 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import sys
-from random import shuffle
 import csv
+from random import shuffle
 
-print sys.argv[1], sys.argv[2]
+# Used for input handling and instructions
+print "for the codes", sys.argv[1], sys.argv[2]
 
-if (bool(sys.argv[1] != "random") & bool(sys.argv[1] != "highest")):
-    print "Error in first argument: As a first argument give either: 'random' - for random sampling or highest or; 'highest' for highest neighbor count first"
+if (bool(sys.argv[1] != "random") & bool(sys.argv[1] != "targeted")):
+    print "Error in first argument: As a first argument give either: 'random' - for random sampling or highest or; 'targeted' for targeted count first"
     print "For example:" 
     print "python matplotlib2.py random india"
     sys.exit(0)
 
-if (bool(sys.argv[2] != "india") & bool(sys.argv[2] != "pensylvania") & bool(sys.argv[2] != "socialnetwork")):
-    print "Error in second argument: As a second argument give either: 'india', 'pensylvania' or 'socialnetwork'"
+if (bool(sys.argv[2] != "india") & bool(sys.argv[2] != "pennsylvania") & bool(sys.argv[2] != "socialnetwork")):
+    print "Error in second argument: As a second argument give either: 'india', 'pennsylvania' or 'socialnetwork'"
     print "For example:" 
     print "python matplotlib2.py random india"
     sys.exit(0)
@@ -32,26 +33,22 @@ if (bool(sys.argv[2] != "india") & bool(sys.argv[2] != "pensylvania") & bool(sys
 if sys.argv[2] == "india":
     codeFile = "INcodes.csv"
     neighborFile = "INneighbors.csv"
-if sys.argv[2] == "pensylvania":
+if sys.argv[2] == "pennsylvania":
     codeFile = "PNcodes.csv"
     neighborFile = "PNneighbors.csv"
 if sys.argv[2] == "socialnetwork":
     codeFile = "NWcodes.csv"
     neighborFile = "NWneighbors.csv"
 
-# THE AMOUNT OF TIMES THAT THE ALGORITHM IS PERFORMED TO CALCULATED THE STATISTICS
+# Variables used to store results of 1000 iterations of map coloring in
 orderedDictionary = {}
 colorAmountList = []
 fourColorResultList = []
 colorAmountListForHistogram = []
-
 for i in range (0, 1000):
     
-    # CLASS DEFINITION
+    # Class definition
     class Province():
-        """
-        Een leeg vak op een kaart
-        """
         def __init__(self, name):
             self.name = name
             self.neighbors = []
@@ -75,19 +72,22 @@ for i in range (0, 1000):
                     break
         
         def visualize(self):
-        	nodes.append(self.name)
-        	colors.append(self.color)
-        	for neighbor in self.neighbors:
-        		edges.append((self.name, neighbor.name)) 
+            nodes.append(self.name)
+            colors.append(self.color)
+            for neighbor in self.neighbors:
+                edges.append((self.name, neighbor.name)) 
 
-    # DIT IS OOK VOOR VISUALISATIE
+    # Variables set for visualisation
     g = nx.Graph()
 
     edges = []
     colors = []
     nodes = []
 
+    # Used for setting the colors
     arrayColor = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+    # Opens CSV files and puts content into dictionaries
     countryDictionary = {}
     with open(codeFile, "r") as landcodes:
         lines = landcodes.readlines()
@@ -115,20 +115,18 @@ for i in range (0, 1000):
     for i in countryDictionary.keys():
         orderedCountryList.append(i)
 
-    
-
     if sys.argv[1] == "random":
         shuffle(orderedCountryList)
 
-    elif sys.argv[1] == "highest":
+    elif sys.argv[1] == "targeted":
 
         # Instantion dictionary with key values ranging up to max neighbor count
-        NeighborCountDict = {}
+        orderedNeighborDict = {}
         for i in orderedCountryList:
             for j in countryDictionary:
                 if i == j:
                     country = countryDictionary.get(j)
-                    NeighborCountDict[len(country.neighbors)] = []
+                    orderedNeighborDict[len(country.neighbors)] = []
 
 
         # Append instantiation of country to proper key depending on neighbor count
@@ -136,14 +134,13 @@ for i in range (0, 1000):
             for j in countryDictionary:
                 if i == j:
                     country = countryDictionary.get(j)
-                    NeighborCountDict[len(country.neighbors)].append(country)
+                    orderedNeighborDict[len(country.neighbors)].append(country)
 
         depthFirstList = []
-        # print NeighborCountDict
-        for i in range (len(NeighborCountDict), 1, -1):
+        for i in range (len(orderedNeighborDict), 1, -1):
             
-            shuffle(NeighborCountDict[i])
-            for j in NeighborCountDict[i]:
+            shuffle(orderedNeighborDict[i])
+            for j in orderedNeighborDict[i]:
                 depthFirstList.append(j)
 
         orderedCountryList = []
@@ -152,19 +149,14 @@ for i in range (0, 1000):
                 if i == countryDictionary.get(j):
                     orderedCountryList.append(j)
 
-    # THIS IS WHERE COLORS ARE SET
+    # Colors are set here
     for j in orderedCountryList:
         (countryDictionary.get(j)).setColor()
-
-        # IS VOOR PRINTEN VAN KLEUREN
         colorList.append(countryDictionary.get(j).color)
-        #print countryDictionary.get(j).color
     colorAmount = max(colorList)
-
-    # prints the minmal amount of numbers
     colorAmountList.append(colorAmount)
 
-    # prints te values and colors when the lowest coloramount has been found (in this case three)
+    # Sets values and colors when the lowest coloramount has been found
     colorOverviewList = []
     if colorAmount == "4": 
         for county in orderedCountryList:
@@ -185,12 +177,11 @@ with open('coloramount.csv', 'wb') as f:
     writer = csv.writer(f)
     writer.writerows(colorAmountListForHistogram)
 
-
 average = sum(intColorAmountList) / len(intColorAmountList)
 print "The average 1000 iterations on this module is", (average + 1)
 print "The minimal amount of colors is", (int(min(colorAmountList)) + 1)
 
-# This is used for the visualistation
+# Code used for visualisation
 visualisationDict = {"0":('black'), "1":('purple'), "2":('yellow'), "3":('red'), "4":('blue'), "5":('green'), "6":('grey')}
 for country in fourColorResultList:
     country.color = visualisationDict[country.color]
